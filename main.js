32e9fbc30fba4d127531cbe86a6a7a1e55f2faf0
BAMTEFA, Olorunshogo Moses <bamtefaolorunshogo12@gmail.com>
2024-03-30 13:49:12 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+ 
+//  Frequently Asked Questions
+    // Onclick opens the sidebar
+    function faqOpen1() {
+        document.getElementById('faq1').style.display = "block";
+    }
+    function faqOpen2() {
+        document.getElementById('faq2').style.display = "block";
+    }
+    function faqOpen3() {
+        document.getElementById('faq3').style.display = "block";
+    }
+    function faqOpen4() {
+        document.getElementById('faq4').style.display = "block";
+    }
+    function faqOpen5() {
+        document.getElementById('faq5').style.display = "block";
+    }
+    function faqOpen6() {
+        document.getElementById('faq6').style.display = "block";
+    }
+
+    // Onclick closes the sidebar
+    function faqClose1() {
+        document.getElementById('faq1').style.display = "none";
+    }
+    function faqClose2() {
+        document.getElementById('faq2').style.display = "none";
+    }
+    function faqClose3() {
+        document.getElementById('faq3').style.display = "none";
+    }
+    function faqClose4() {
+        document.getElementById('faq4').style.display = "none";
+    }
+    function faqClose5() {
+        document.getElementById('faq5').style.display = "none";
+    }
+    function faqClose6() {
+        document.getElementById('faq6').style.display = "none";
+    }

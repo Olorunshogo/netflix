8e36a0972f9ae7f317667873ca4e5b5ce3867260
BAMTEFA Olorunshogo Moses <shownzy001@gmail.com>
2024-05-27 19:29:12 +0100

    The language button is next. I'm done editing here.

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# netflix
+# Netflix
+## Description
+## Table of content
+## Features
+### Features I'll like to work on in the future
+## Credits
--- main.js
+++ main.js
@@ -1,41 +1,59 @@
  
-//  Frequently Asked Questions
-    // Onclick opens the sidebar
-    function faqOpen1() {
-        document.getElementById('faq1').style.display = "block";
-    }
-    function faqOpen2() {
-        document.getElementById('faq2').style.display = "block";
-    }
-    function faqOpen3() {
-        document.getElementById('faq3').style.display = "block";
-    }
-    function faqOpen4() {
-        document.getElementById('faq4').style.display = "block";
-    }
-    function faqOpen5() {
-        document.getElementById('faq5').style.display = "block";
-    }
-    function faqOpen6() {
-        document.getElementById('faq6').style.display = "block";
-    }
+//  FAQ SECTION
 
-    // Onclick closes the sidebar
-    function faqClose1() {
-        document.getElementById('faq1').style.display = "none";
-    }
-    function faqClose2() {
-        document.getElementById('faq2').style.display = "none";
-    }
-    function faqClose3() {
-        document.getElementById('faq3').style.display = "none";
-    }
-    function faqClose4() {
-        document.getElementById('faq4').style.display = "none";
-    }
-    function faqClose5() {
-        document.getElementById('faq5').style.display = "none";
-    }
-    function faqClose6() {
-        document.getElementById('faq6').style.display = "none";
-    }
+// Onclick opens the answer DIV
+function faqOpen1() {
+    document.getElementById("additionIcon1").style.display = "none";
+    document.getElementById('faq1').style.display = "block";
+}
+
+function faqOpen2() {
+    document.getElementById("additionIcon2").style.display = "none";
+    document.getElementById('faq2').style.display = "block";
+}
+
+function faqOpen3() {
+    document.getElementById("additionIcon3").style.display = "none";
+    document.getElementById('faq3').style.display = "block";
+}
+
+function faqOpen4() {
+    document.getElementById("additionIcon4").style.display = "none";
+    document.getElementById('faq4').style.display = "block";
+}
+
+function faqOpen5() {
+    document.getElementById("additionIcon5").style.display = "none";
+    document.getElementById('faq5').style.display = "block";
+}
+
+function faqOpen6() {
+    document.getElementById("additionIcon6").style.display = "none";
+    document.getElementById('faq6').style.display = "block";
+}
+
+ // Onclick closes the hidden div
+ function faqClose() {
+    document.getElementById('faq1').style.display = "none";
+    document.getElementById('faq2').style.display = "none";
+    document.getElementById('faq3').style.display = "none";
+    document.getElementById('faq4').style.display = "none";
+    document.getElementById('faq5').style.display = "none";
+    document.getElementById('faq6').style.display = "none";
+    document.getElementById("additionIcon1").style.display = "block";
+    document.getElementById("additionIcon2").style.display = "block";
+    document.getElementById("additionIcon3").style.display = "block";
+    document.getElementById("additionIcon4").style.display = "block";
+    document.getElementById("additionIcon5").style.display = "block";
+    document.getElementById("additionIcon6").style.display = "block";
+
+    // Footer Language Div
+    document.getElementById('faq6').style.display = "none";
+    document.getElementById("additionIcon6").style.display = "block";
+}
+
+// FOOTER Language Div
+function langsOpen() {
+    document.getElementById("additionIcon6").style.display = "none";
+    document.getElementById('faq6').style.display = "block";
+}
